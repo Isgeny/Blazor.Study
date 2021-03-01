e0b8f3b2c51756aa592348f8f0273e4041f1b0d5
Isgeny <jack091196@gmail.com>
2021-03-01 20:59:31 +0300

    Study Hello Blazor Server App

---
+++ HelloBlazorServerApp/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloBlazorServerApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(x => x.UseStartup<Startup>())
+                .Build()
+                .Run();
+        }
+    }
+}
